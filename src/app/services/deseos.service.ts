ceaa8fe0e57d2783208dc35e7a15727ba017da98
Gustavo Herrera <gustavoh.2312@gmail.com>
2019-05-29 10:53:15 -0500

    muestreo de lista de deseos, configuracion de styles

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -10,7 +10,14 @@ export class DeseosService {
 
 
   constructor() {
-    console.log('servicio inicializado');
+ 
+     const lista1 = new Lista('Recolectar piedras del infinito');
+     const lista2 = new Lista('Heroes a desaparecer');
+
+     this.listas.push(lista1,lista2);
+
+     console.log(this.listas);
+
    }
 
 
